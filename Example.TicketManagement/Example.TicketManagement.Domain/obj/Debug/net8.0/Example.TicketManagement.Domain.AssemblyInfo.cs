59384011047912291a31ae815b528253d797a2fb
Konstantin Dinev <konstantin.dinev@accedia.com>
2024-07-02 15:40:01 +0300

    Finish Domain Project

--- Example.TicketManagement/Example.TicketManagement.Domain/obj/Debug/net8.0/Example.TicketManagement.Domain.AssemblyInfo.cs
+++ Example.TicketManagement/Example.TicketManagement.Domain/obj/Debug/net8.0/Example.TicketManagement.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Example.TicketManagement.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1eb9b78cdb9fd67de474ed2bc8bf5c84174a551")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+229242ba26a2e1d83bd2dd4614c1264fb3646ab5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Example.TicketManagement.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Example.TicketManagement.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
