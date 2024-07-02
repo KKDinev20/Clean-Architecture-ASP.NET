b70484b498f356634a10de904142c87ed6a7d08d
Konstantin Dinev <konstantin.dinev@accedia.com>
2024-07-02 16:28:06 +0300

    Create Application project and implement Contracts

---
+++ Example.TicketManagement/Example.TicketManagement.Application/Contracts/Persistence/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using GloboTicket.TicketManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example.TicketManagement.Application.Contracts.Persistence
+{
+    internal interface ICategoryRepository : IAsyncRepository<Category>
+    {
+    }
+}
